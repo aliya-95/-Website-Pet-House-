c87c530dafc91c197dcf10a557e6021a5ab6a7f5
Aliya Safiullina <112345960+aliya-95@users.noreply.github.com>
2022-11-03 02:17:01 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+from pets import Cat
+
+cat_1 = Cat("name: Sam", "gender: boy", "age: 2 years")
+cat_2 = Cat("name: Jack", "gender: boy", "age: 5 years")
+cat_3 = Cat("name: Liza", "gender: girl", "age: 1 year")
+
+print(cat_1.getName(), cat_1.getGender(), cat_1.getAge())
+print(cat_2.getName(), cat_2.getGender(), cat_2.getAge())
+print(cat_3.getName(), cat_3.getGender(), cat_3.getAge())
